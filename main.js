1e14e06f1a3bbc5ce25884609bae6248f73a50a5
Lucas Hild <lucas@hild-stuttgart.de>
2017-07-25 13:03:29 +0200

    Use animations

--- main.js
+++ main.js
@@ -1,4 +1,43 @@
 $(document).ready(function () {
+  // Newsletter form
+  $("#newsletter-form").on("submit", function(event) {
+
+    // Hide (old) alert boxes
+    $("#newsletter-success").hide();
+    $("#newsletter-error").hide();
+    $("#newsletter-success p").text("");
+    $("#newsletter-error p").text("");
+
+    // Post data to api
+    $.ajax({
+      data: {
+        source: $("#newsletter-source").val(),
+        mail: $("#newsletter-mail").val(),
+      },
+      type: "POST",
+      url: "https://lanseuo.herokuapp.com/newsletter/subscribe"
+    })
+
+    .done(function(data) {
+      if (data.error) {
+        $("#newsletter-error").fadeIn(500);
+        $("#newsletter-error").css("display", "block");
+        $("#newsletter-error p").append(data.error);
+      }
+      else {
+        $("#newsletter-success").fadeIn(500);
+        $("#newsletter-success").css("display", "block");
+        $("#newsletter-success p").append("Subscribed successfully!");
+
+        // Clear input fields
+        $("#newsletter-mail").val("");
+      };
+    });
+
+    event.preventDefault();
+  });
+
+  // Contact Form
   $("#contact-form").on("submit", function(event) {
 
     // Hide (old) alert boxes
@@ -15,17 +54,17 @@ $(document).ready(function () {
         message: $("#contact-message").val()
       },
       type: "POST",
-      url: "https://lucas-hild.de/fcgi-bin/api.fcgi/mail-contact"
+      url: "https://lanseuo.herokuapp.com/mail-contact"
     })
 
     .done(function(data) {
       if (data.error) {
-        $("#contact-error").show();
+        $("#contact-error").fadeIn(500);
         $("#contact-error").css("display", "block");
         $("#contact-error p").append(data.error);
       }
       else {
-        $("#contact-success").show();
+        $("#contact-success").fadeIn(500);
         $("#contact-success").css("display", "block");
         $("#contact-success p").append("Message sent successfully!");
 
@@ -41,6 +80,6 @@ $(document).ready(function () {
 
   // Close alert if button (x) is pressed
   $(".closebtn").click(function () {
-    $(this).parent().hide();
+    $(this).parent().fadeOut(500);
   });
 });
