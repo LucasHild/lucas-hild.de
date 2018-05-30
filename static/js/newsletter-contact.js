78687b35d021a24e32b3640917ac22b7453f371b
Lucas Hild <contact@lucas-hild.de>
2018-05-30 14:15:50 +0200

    Update newsletter form to Mailerlite

--- static/js/newsletter-contact.js
+++ static/js/newsletter-contact.js
@@ -1,44 +1,4 @@
 $(document).ready(function () {
-    // Newsletter form
-    $('#newsletter-form').on('submit', function (event) {
-        $('#newsletter-spinner').show();
-
-        // Hide (old) alert boxes
-        $('#newsletter-success').hide();
-        $('#newsletter-error').hide();
-        $('#newsletter-success p').text('');
-        $('#newsletter-error p').text('');
-
-        // Post data to api
-        $.ajax({
-            data: {
-                source: $('#newsletter-source').val(),
-                mail: $('#newsletter-mail').val(),
-            },
-            type: 'POST',
-            url: 'https://lanseuo.herokuapp.com/newsletter/subscribe',
-        })
-
-            .done(function (data) {
-                $('#newsletter-spinner').hide();
-
-                if (data.error) {
-                    $('#newsletter-alert').css('border', '3px solid rgb(218, 13, 61)');
-                    $('#newsletter-alert p').text(data.error);
-                } else if (data.success == 'subscribed') {
-                    $('#newsletter-alert').css('border', '3px solid rgb(66, 181, 131)');
-                    $('#newsletter-alert p').append('Vielen Dank fürs Anmelden!');
-
-                    // Clear input fields
-                    $('#newsletter-mail').val('');
-                }
-                $('#newsletter-alert').fadeIn(500);
-                $('#newsletter-alert').css('display', 'block');
-            });
-
-        event.preventDefault();
-    });
-
     // Contact Form
     $('#contact-form').on('submit', function (event) {
         $('#contact-spinner').show();
