9f35e546f3db61bd7cec127d3a501a39cabba1ad
Lucas Hild <contact@lucas-hild.de>
2019-03-04 21:11:36 +0100

    Add programming section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,6 +6,11 @@
             <h2>About me</h2>
             <p>I'm a guy from Germany who loves technology in every form. Learning new things every day is what I enjoy most.</p>
         </TextImage>
+
+        <TextImage inversed :image="require('@/assets/images/programming.webp')">
+            <h2>Programming is my passion</h2>
+            <p>I learned to program with my Raspberry Pi back when I was 14 years old. Since then I learned several programming languages and developed some side projects. Currently I am developing in Python, JavaScript (with Vue.js) and Go.</p>
+        </TextImage>
     </div>
 </template>
 
