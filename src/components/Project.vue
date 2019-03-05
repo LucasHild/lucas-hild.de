06a36baca47ae1e070bda92898010c81eb0d8449
Lucas Hild <contact@lucas-hild.de>
2019-03-05 20:51:30 +0100

    Put project in seperate component

---
+++ src/components/Project.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="project" :style="`background-image: url(${image}); --color: ${color}`">
+        <div class="content">
+            <p>{{ text }}</p>
+            <a :href="link" class="link" target="_blank">Visit</a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'project',
+
+    props: ['image', 'color', 'text', 'link']
+}
+</script>
+
+<style scoped>
+.project {
+    color: white;
+    height: 250px;
+    padding: 10px;
+    text-align: center;
+
+    background-size: contain;
+    background-position: center;
+
+    display: grid;
+    place-items: center;
+
+    position: relative;
+    z-index: 5;
+}
+
+.project:after {
+    content: "";
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background: var(--color);
+    opacity: 0;
+
+    transition: all 0.5s ease-in-out;
+    z-index: 1;
+}
+.project:hover:after {
+    z-index: 1;
+    opacity: 1;
+}
+
+.project .content {
+    z-index: 5;
+    transition: all 0.5s ease-in-out;
+    opacity: 0;
+}
+
+.project:hover .content {
+    opacity: 1;
+}
+
+.project .content .link {
+    color: white;
+    border: 1px solid white;
+    padding: 5px 50px;
+    border-radius: 5px;
+}
+
+.project .content .link:hover {
+    background: white;
+    color: var(--color);
+}
+</style>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -3,48 +3,27 @@
         <h2>My recent Projects</h2>
 
         <div class="grid">
-            <div
-                class="project"
-                :style="`background-image: url(${require('@/assets/images/projects/schoolsen.webp')})`"
-            >
-                <div class="content">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-                    <a href="https://schoolsen.de" class="link" target="_blank">Visit</a>
-                </div>
-            </div>
-
-            <div
-                class="project"
-                :style="`background-image: url(${require('@/assets/images/projects/schoolsen.webp')})`"
-            >
-                <div class="content">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-                    <a href="https://schoolsen.de" class="link" target="_blank">Visit</a>
-                </div>
-            </div>
-
-            <div
-                class="project"
-                :style="`background-image: url(${require('@/assets/images/projects/schoolsen.webp')})`"
-            >
-                <div class="content">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-                    <a href="https://schoolsen.de" class="link" target="_blank">Visit</a>
-                </div>
-            </div>
+            <Project :image="require('@/assets/images/projects/schoolsen.webp')" color="#008a75" text="Lorem ipsum dolor sit amet consectetur adipisicing." link="https://schoolsen.de"/>
+            <Project :image="require('@/assets/images/projects/schoolsen.webp')" color="blue" text="Lorem ipsum dolor sit amet consectetur adipisicing." link="https://schoolsen.de"/>
+            <Project :image="require('@/assets/images/projects/schoolsen.webp')" color="#008a75" text="Lorem ipsum dolor sit amet consectetur adipisicing." link="https://schoolsen.de"/>
         </div>
     </section>
 </template>
 
 <script>
+import Project from '@/components/Project'
+
 export default {
-    name: 'projects'
+    name: 'projects',
+
+    components: { Project }
 }
 </script>
 
 <style scoped>
 h2 {
     text-align: center;
+    margin-bottom: 50px;
 }
 
 .grid {
@@ -53,66 +32,5 @@ h2 {
     grid-gap: 25px;
     max-width: 1200px;
     margin: 0 auto;
-}
-
-.project {
-    color: white;
-    height: 250px;
-    padding: 10px;
-    text-align: center;
-
-    background-size: contain;
-    background-position: center;
-
-    display: grid;
-    place-items: center;
-
-    position: relative;
-    z-index: 5;
-}
-
-/*  */
-.project:after {
-    content: "";
-    position: absolute;
-    left: 0;
-    top: 0;
-    width: 100%;
-    height: 100%;
-    background: #008a75;
-    opacity: 0;
-
-    transition: all 0.5s ease-in-out;
-    z-index: 1;
-}
-.project:hover:after {
-    z-index: 1;
-    opacity: 1;
-}
-/*  */
-.project:hover {
-    /* background-image: none !important; */
-}
-
-.project .content {
-    z-index: 5;
-    transition: all 0.5s ease-in-out;
-    opacity: 0;
-}
-
-.project:hover .content {
-    opacity: 1;
-}
-
-.project .content .link {
-    color: white;
-    border: 1px solid white;
-    padding: 5px 50px;
-    border-radius: 5px;
-}
-
-.project .content .link:hover {
-    background: white;
-    color: #008a75;
 }
 </style>
