be7e2888f3a5d96571f0759a52506ce228b02384
Lucas Hild <contact@lucas-hild.de>
2019-06-17 18:36:46 +0200

    Add section 'Blog'

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,18 @@
     <div class="home">
         <Header/>
         <AboutMe/>
+        <Blog/>
     </div>
 </template>
 
 <script>
 import Header from '@/components/Header'
 import AboutMe from '@/components/sections/AboutMe'
+import Blog from '@/components/sections/Blog'
 
 export default {
     name: 'home',
 
-    components: { Header, AboutMe }
+    components: { Header, AboutMe, Blog }
 }
 </script>
