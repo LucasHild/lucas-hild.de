c71e59545888d09bfbad235790f0ee70a2346ab4
Lucas Hild <contact@lucas-hild.de>
2019-07-05 19:24:02 +0200

    Add css

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+import '@/assets/css/settings.css'
+import '@/assets/css/generic.css'
+import '@/assets/css/elements.css'
+import '@/assets/css/objects.css'
+import '@/assets/css/utilities.css'
+
 new Vue({
     router,
     render: h => h(App)
