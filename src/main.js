e977193ecbc29b27955d34602a3a94d7bdfd2483
Lucas Hild <contact@lucas-hild.de>
2019-07-07 17:38:44 +0200

    Add Google Analytics

--- src/main.js
+++ src/main.js
@@ -4,6 +4,27 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+import VueAnalytics from 'vue-analytics'
+Vue.use(VueAnalytics, {
+    id: process.env.VUE_APP_GOOGLE_ANALYTICS_ID,
+    router,
+    autoTracking: {
+        pageviewTemplate(route) {
+            return {
+                page: route.path,
+                title: route.name,
+                location: window.location.href
+            }
+        }
+    },
+    set: [
+        { field: 'anonymizeIp', value: true }
+    ],
+    debug: {
+        sendHitTask: process.env.NODE_ENV === 'production'
+    }
+})
+
 import '@/assets/css/settings.css'
 import '@/assets/css/generic.css'
 import '@/assets/css/elements.css'
